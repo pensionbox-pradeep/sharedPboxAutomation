34f05c018a909f6257f6cd3b0bf86d1d38a48e81
Dewesh Pandey <dewesh@pensionbox.in>
2024-08-07 15:02:44 +0530

    new file from dewesh

---
+++ src/main/java/generic/DeweshCollab.java
@@ -0,0 +1,11 @@
+package generic;
+
+import org.apache.commons.collections4.bag.SynchronizedSortedBag;
+
+public class DeweshCollab {
+
+	public static void main(String[] args) {
+		System.out.println("New File from Dewesh");
+	}
+
+}
